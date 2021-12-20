a78aa71cbcfeb0db5ccb772dc75bf385f611b589
petarvucinic <87143624+petarvucinic@users.noreply.github.com>
2021-12-20 11:44:15 +0100

    initial commit

---
+++ vezba.js
@@ -0,0 +1,66 @@
+/*
+// za ispis 
+console.log("hello world");
+
+neka_promenljiva = 10;
+
+
+// dek
+// global scope
+var promenljiva;
+
+// def
+promenljiva = 10;
+
+// local scope
+let promenljiva2 = 10;
+
+const promenljiva3;
+promenljiva3 = 5
+
+var bool 
+// undefined
+
+//  ==(vrednosti) i ===(tip)
+// != i !==
+*/
+
+
+
+// ===========================================================================
+
+
+
+// FARH U CELZ
+
+let cel;
+let fah;
+fah = 45;
+
+cel = (fah - 32)*5/9;
+console.log(cel + "°C");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
