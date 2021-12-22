d7fa37f511f582639123d4e8f58a4b662f720786
petarvucinic <87143624+petarvucinic@users.noreply.github.com>
2021-12-22 11:39:40 +0100

    js

--- js/vezba.js
+++ js/vezba.js
@@ -33,34 +33,160 @@ var bool
 
 // FARH U CELZ
 
-let cel;
-let fah;
-fah = 45;
+// let cel;
+// let fah;
+// fah = 45;
 
-cel = (fah - 32)*5/9;
-console.log(cel + "°C");
-
-
+// cel = (fah - 32)*5/9;
+// console.log(cel + "°C");
 
 
 
-
+// ============================================================================
 
 
 
 
+// if (true) {
+//     console.log("bla ")
+// } else if () {
+//     console.log("bla bla")
+// } else {
+//     console.log("bla bla bla")
+// } 
+
+
+
+// ==========================================================================
 
 
 
 
+// var broj = 32;
+
+// if(broj > 0) {
+//     console.log("pozitivan")
+// } else if (broj < 0) {
+//     console.log("negativan")
+// }
+// else {
+//     console.log("broj je nula")
+// }
 
 
 
 
+// ===========================================================================
 
 
 
 
+// var broj1 = 3;
 
+// if(broj1%3==0 && broj1%5==0){
+//     console.log("fizzbuzz");
+// } else if(broj1%3==0){
+//     console.log("fizz");
+// } else if(broj1%5==0) {
+//     console.log("buzz")
+// }
+// else {
+//     console.log(broj1);
+// }
+
+
+
+// =============================================================================
+
+
+
+
+// var broj1 = 5;
+
+// if(broj1%2==0){
+//     console.log("paran je");
+// } else {
+//     console.log("nije paran");
+// }
+
+
+
+
+// -----------------------------------------------------------------------------
+
+
+
+
+// var broj1 = 15;
+// var broj2 = 40;
+
+// if(broj1 == 50 || broj2 == 50){
+//     console.log("true");
+// } else if (broj1 + broj2 >= 50) {
+//     console.log("true");
+// } else {
+//     console.log("false");
+// }
+
+
+
+
+// ==============================================================================
+
+
+
+// var num = 1500;
+
+// if(num >= 100 && num <=200){
+//     console.log("nalazi se izmedju 100 i 200");
+// } else {
+//     console.log("ne nalazi se izmedju 100 i 200");
+// }
+
+
+
+
+// ==============================================================================
+
+
+
+// var godina = 2000;
+
+// if(godina %400==0){
+//     console.log("prestupna je");
+// } 
+// else {
+//     console.log("nije prestupna");
+// }
+
+
+
+// -------------------------------------------------------------------------------
+
+
+
+var num1 = 2;
+var num2 = 3;
+var num3 = 4;
+var num4 = 1;
+var num5 = 0;
+
+
+if(num1 > num2 && num1 > num3 && num1 > num4 && num1 > num5) {
+    console.log("prvi br=", num1);
+} else if(num2 > num1 && num2 > num3 && num2 > num4 && num2 > num5){
+    console.log("drugi br=", num2);
+} else if(num3 > num1 && num3 > num2 && num3 > num4 && num3 > num5){
+    console.log("treci br=", num3);
+} else if(num4 > num1 && num4 > num2 && num4 > num3 && num4 > num5){
+    console.log("cetvrti br=", num4);
+} else {
+    console.log("peti br=", num5);
+}
+
+
+
+
+// ============================================================================
 
 
