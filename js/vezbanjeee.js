f0e2546657dd094c25cba0b07a0912c33912a7b5
petarvucinic <87143624+petarvucinic@users.noreply.github.com>
2022-04-02 13:46:44 +0200

    commit

--- js/vezbanjeee.js
+++ js/vezbanjeee.js
@@ -67,8 +67,6 @@
 
 // --------------------------------------------------
 
-
-
 // const niz = [
 //   [1, 0, 0],
 //   [0, 1, 0],
@@ -88,11 +86,7 @@
 
 // console.log(pr(niz))
 
-
-
 // ------------------------------------------------
-
-
 
 // function spl(arr){
 //   even = []
@@ -108,33 +102,169 @@
 //   }
 // }
 
+// console.log(spl([2, 3, 7, 6, 2, 4, 9]))
+
+// ---------------------
+
+// const mix = (ar1, ar2) => {
+//   res = []
+//   for(let i = 0; i < ar1.length + ar2.length; i++){
+//     if(ar1[i] !== undefined){
+//       res.push(ar1[i])
+//     }
+//     if(ar2[i] !== undefined){
+//       res.push(ar2[i])
+//     }
+//   }
+//   return res
+// }
+
+// niz = ["a", "b", "c"];
+// niz2 = [1, 3, 4, 5];
+// console.log(mix(niz, niz2));
+
+// ---------------------------------
+
+// const niz = [
+//   [1, 3, 6],
+//   [4, 9, 1],
+//   [4, 1, 9],
+// ]
+
+// for(let i = 0; i < niz.length; i++){
+//   niz[i][niz.length - 1 - i] = 0
+// }
+
+// console.log(niz)
+
+// ---------------------------
+
+// const niz = [
+//   [1, 3, 6],
+//   [4, 9, 1],
+//   [4, 1, 9],
+// ]
+
+// for(let i = 0; i < niz.length; i++){
+//   for(let j = 0; j < niz.length; j++){
+//     if(i === j){
+//       niz[i][j] = 0
+//     }
+//   }
+// }
+
+// console.log(niz)
+
+// -----------------------------
+
+// const niz = [
+//   [1, 0, 0],
+//   [0, 0, 0],
+//   [0, 0, 1],
+// ];
+
+// const fn = (niz) => {
+//   for (let i = 0; i < niz.length; i++) {
+//     for (let j = 0; j < niz.length; j++) {
+//       if ((i === j && niz[i][j] !== 1) || (i !== j && niz[i][j] !== 0)) {
+//         return false;
+//       }
+//     }
+//   }
+//   return true;
+// };
+
+// console.log(fn(niz));
+
+
+
+// --------------
+
+
+// const fnod = (arr) => {
+//   for(let i = 0; i < arr.length; i++){
+//     for(let j = 0; j < arr.length; j++){
+//       if(arr[0] !== arr[1] && arr[1] === arr[2]){
+//         return arr[0]
+//       } else if (arr[i] !== arr[i + 1]){
+//         return arr[i + 1]
+//       }
+//       }
+//     }
+//   }
+
+
+
+// console.log(fnod(["B", "A", "A", "A", ]))
+
+
+// ------------------------------
+
+
+// const spl = (arr) => {
+//   e = []
+//   o = []
+//   for(let x of arr){
+//     if(x % 2 == 0 && !arr.includes(x)){
+//       e.push(x)
+//     } else if(x % 2 !== 0 && !arr.includes(x)){
+//       o.push(x)
+//     }
+//   }
+//   return e, o
+// }
+
 
 // console.log(spl([2, 3, 7, 6, 2, 4, 9]))
 
 
 
-
-// ---------------------
-
+// ----------------------
 
 
-const mix = (ar1, ar2) => {
-  res = []
-  for(let i = 0; i < ar1.length + ar2.length; i++){
-    if(ar1[i] !== undefined){
-      res.push(ar1[i])
-    }
-    if(ar2[i] !== undefined){
-      res.push(ar2[i])
-    }
-  }
-  return res
-}
 
 
-niz = ["a", "b", "c"];
-niz2 = [1, 3, 4, 5];
-console.log(mix(niz, niz2));
+// niz = ["a", "b", "c"];
+// niz2 = [1, 3, 4, 5];
+
+// const mix = (niz, niz2) => {
+//   res = []
+//   for(let i = 0; i < niz.length + niz2.length; i++){
+//     if(niz[i] !== undefined){
+//       res.push(niz[i])
+//     } 
+//     if (niz2[i] !== undefined){
+//       res.push(niz2[i])
+//     }
+//   }
+//   return res
+// }
+
+// console.log(mix(niz, niz2));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
