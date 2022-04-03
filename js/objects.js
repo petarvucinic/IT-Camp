0ea153beae76328ef98298a03b5fb3b71a522fc7
petarvucinic <87143624+petarvucinic@users.noreply.github.com>
2022-04-03 14:54:31 +0200

    obj

--- js/objects.js
+++ js/objects.js
@@ -1,2 +1,112 @@
 
-console.log("objekti")
+// // console.log("objekti")
+
+
+// // let person = "ajsa"
+
+
+// const person = {
+//     name: "pero",
+//     lastname: "vucinic",
+//     age: 17,
+//     arr : [
+//         1, 2, 3, 4
+//     ],
+//     fullName : function(){
+//         return this.name + this.lastname
+//     }
+// }
+
+
+// console.log(person.name)
+// console.log(person["name"])
+
+
+// console.log(person.fullName(person.name, person.lastname))
+// console.log(person.fullName("Fatih", "l"))
+// console.log(person.fullName())
+
+
+// person.heigth = 170
+// person.age = 20
+
+// console.log(person)
+
+
+
+// konstruktor
+// let obj = new Object()
+
+// obj.a  = "A"
+// obj.b  = "b"
+
+// console.log(obj)
+
+
+
+
+// const person = {
+//     name: "pero",
+//     lastname: "vucinic",
+//     age: 17,
+//     arr : [
+//         1, 2, 3, 4
+//     ],
+//     fullName : function(){
+//         return this.name + this.lastname
+//     }
+// }
+
+
+
+// for(let i in person){
+//     console.log(i, person[i])
+// }
+
+
+
+// ----
+
+
+
+
+// const person = {
+//     name: "pero",
+//     lastname: "vucinic",
+//     age: 17,
+//     arr : [
+//         1, 2, 3, 4
+//     ],
+//     fullName : function(){
+//         return this.name + this.lastname
+//     },
+//     tr : 2022,
+//     godr : function(){
+//         return `vasa godina rodjenja je ${this.tr - this.age}`
+//     } 
+// }
+
+// console.log(person.godr())
+
+
+// for(let i in person){
+//     if(person[i] === person.age && person.age > 18){
+//         console.log("punoletni")
+        
+//     } else if (person[i] === person.age && person.age < 18){
+//         console.log("niste punoletni")
+//     }
+// }
+
+
+
+// -------------------------
+
+
+
+var 
+
+
+
+
+
