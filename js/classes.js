4125d2f5f82eb39d27d1021a3dad8328689df024
petarvucinic <87143624+petarvucinic@users.noreply.github.com>
2022-04-10 14:36:21 +0200

    classes

--- js/classes.js
+++ js/classes.js
@@ -1,15 +1,26 @@
 
 
 class Person {
-    constructor(name, lastname, age){
-        this.name = name
-        this.lastname = lastname
-        this.age = age
+    constructor(name, lastname, age, method1){
+        this.name = name;
+        this.lastname = lastname;
+        this.age = age;
+        this.method1 = method1
+    }
+    birthYear = () => {return 2022 - this.age}
+    static test(){
+        return "hello"
     }
 }
 
 
+let pers1 = new Person("Pero", "Vuc", 17, ()=>{return "132456789"})
 
+// let pers2 = new Person("Ajsa", "Bes", 17)
+console.log(pers1)
+console.log(pers1.birthYear())
+console.log(Person.test())
+// console.log(pers2)
 
 
 
