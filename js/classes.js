5e0592ac453fa38cc6c11407f678f69447377735
petarvucinic <87143624+petarvucinic@users.noreply.github.com>
2022-04-10 14:13:05 +0200

    classes

---
+++ js/classes.js
@@ -0,0 +1,27 @@
+
+
+class Person {
+    constructor(name, lastname, age){
+        this.name = name
+        this.lastname = lastname
+        this.age = age
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
