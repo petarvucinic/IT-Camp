eea87c5233b9e28bbcc060fa9a9d3bd37da2163d
petarvucinic <87143624+petarvucinic@users.noreply.github.com>
2022-04-17 22:00:58 +0200

    commit

---
+++ js/test.js
@@ -0,0 +1,99 @@
+// suma = 0
+// for(let i = 0; i < 101; i++){
+//     suma += i**i
+// }
+
+// pom2 = " "
+
+// for(i = 0; i < 11; i++){
+//     pom2 += suma[i].toString()
+// }
+
+
+// console.log(pom2)
+
+
+// console.log("4" + "8")
+
+
+
+
+// n = 2**1000
+// suma = 0
+
+// for(let i = 0; i < (n.toString()).length; i++){
+//     suma += (n[i]).toInt()
+// }
+
+// console.log(suma)
+
+
+// --
+
+
+
+
+// for(let a = 1; a < 1000; a++){
+//     for(let b = 1; b < 1000; b++){
+//         for(let c = 1; c < 1000; c++){
+//             if (a<b && a < c && b < c && a**2 + b**2 == c**2 && a + b + c === 1000){
+//                 console.log(a*b*b)
+//                 break
+//             }
+//         }
+//     }
+// }
+
+
+// ---------------
+
+
+// for(let i = 1; i < 101; i++){
+//     prod *= i
+// }
+
+// console.log
+
+
+// -----
+// console.log("1" * 5)
+
+
+// ------
+
+// []
+// pom = []
+// res = []
+// for(let i = 0; i < 4; i++){
+//     pom.push(1)
+//     console.log(pom)
+//     res.push(pom)
+// }
+
+// console.log(res)
+
+
+
+// ---------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
