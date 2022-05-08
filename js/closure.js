b2b3eaabd0840874200a7deaa0fb7c74c01af890
petarvucinic <87143624+petarvucinic@users.noreply.github.com>
2022-05-08 14:28:40 +0200

    commit

--- js/closure.js
+++ js/closure.js
@@ -1,22 +1,42 @@
+// let count = 7
+
+// function foo() {
+//   let count = 0;
+//   console.log(count);
+// }
+
+// foo();
+// console.log(count)
+
+// function outerFunc() {
+//     let outerVar = "I am outside"
+//     function innerFunc() {
+//         console.log(outerVar)
+//     }
+
+//     innerFunc()
+// }
+
+// outerFunc()
+
+// --------------
+
+const myGlobal = 0;
+
+function func() {
+  const myVar = 1;
+  console.log(myGlobal);
+  function innerofFunc() {
+    const myInnerVar = 2;
+    console.log(myVar, myGlobal);
+    function innerOfInnerOfFunc() {
+      console.log(myInnerVar, myVar, myGlobal);
+    }
+    innerOfInnerOfFunc();
+  }
+  innerofFunc();
+}
+func();
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// ---------------------------
