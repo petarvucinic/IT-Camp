0931820500e0128235f07480947ace0554afdc50
petarvucinic <87143624+petarvucinic@users.noreply.github.com>
2022-05-15 14:50:11 +0200

    nzm

--- js/closure.js
+++ js/closure.js
@@ -21,22 +21,71 @@
 
 // --------------
 
-const myGlobal = 0;
+// const myGlobal = 0;
 
-function func() {
-  const myVar = 1;
-  console.log(myGlobal);
-  function innerofFunc() {
-    const myInnerVar = 2;
-    console.log(myVar, myGlobal);
-    function innerOfInnerOfFunc() {
-      console.log(myInnerVar, myVar, myGlobal);
-    }
-    innerOfInnerOfFunc();
-  }
-  innerofFunc();
-}
-func();
-
+// function func() {
+//   const myVar = 1;
+//   console.log(myGlobal);
+//   function innerofFunc() {
+//     const myInnerVar = 2;
+//     console.log(myVar, myGlobal);
+//     function innerOfInnerOfFunc() {
+//       console.log(myInnerVar, myVar, myGlobal);
+//     }
+//     innerOfInnerOfFunc();
+//   }
+//   innerofFunc();
+// }
+// func();
 
 // ---------------------------
+
+// function outerFunc() {
+//   let outerVar = "I am outside";
+//   function innerFunc() {
+//     console.log(outerVar);
+//   }
+//   return innerFunc;
+// }
+
+// function execute() {
+//   const myInnerFunc = outerFunc();
+//   myInnerFunc();
+// }
+
+// execute();
+
+
+// --------------------------------
+
+
+// var count = 1;
+// const func = () => {
+//     console.log(count)
+
+// }
+
+// func()
+// console.log(count)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
