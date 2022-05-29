05c705f004bd0850dc95d33603e49620dfa7f6cd
petarvucinic <87143624+petarvucinic@users.noreply.github.com>
2022-05-29 14:52:05 +0200

    commit

--- js/array_methods.js
+++ js/array_methods.js
@@ -4,10 +4,7 @@
 
 // console.log(arr2)
 
-
-
 // -------------------
-
 
 // function cap(arr){
 //     res = arr.map(el => el = el.replace(el[0], el[0].toUpperCase()))
@@ -18,19 +15,13 @@
 
 // cap(["hff", "vugdv", "ud8uhx", "vDDv"])
 
-
-
 // -----------------------------------
-
-
-
 
 // const arr = [1, 2, 3, 4]
 
 // const sum = arr.reduce((prev, curr) => prev + curr)
 
 // console.log(sum)
-
 
 // ----------------
 
@@ -40,14 +31,64 @@
 
 // console.log(res)
 
-
-
 // -----------------------------
 
+// arr = [[0, 1], [2, 3], [4, 5]]
+// int = []
+// res = arr.reduce((pv, cv) =>  pv.concat(cv), [])
 
-arr = [[0, 1], [2, 3], [4, 5]]
-int = []
-res = arr.reduce((pv, cv) =>  pv.concat(cv), [])
+// console.log(res)
+
+// ---------------------------------
+
+// const friends = [
+//   {
+//     name: "Anna",
+//     books: ["Bible", "Harry Potter"],
+//     age: 21,
+//   },
+//   {
+//     name: "Bob",
+//     books: ["War and peace", "Romeo and JUliet"],
+//     age: 21,
+//   },
+//   {
+//     name: "Alice",
+//     books: ["The lord of the rings", "The shinig"],
+//     age: 21,
+//   },
+// ];
+
+// int = ["Alphabet"]
+
+// // res = friends.reduce((pv, cv) => pv.concat(cv.books), int)
+// // res = friends.reduce((pv, cv)=> {return [...pv, ...cv.books]}, int)
+
+// // console.log(res)
+
+// izolacija
+// const obj = {
+//     name : 'asja'
+// }
+// const {name} = obj;
+// console.log(name)
+
+// --------------------
+
+// const myArray = ["a", "a", "b", "b", "c", "c", "b", "b", "d", "d", "d"];
+
+// const arr = myArray.reduce((pv, cv) => {
+//   if (!pv.includes(cv)) {
+//     return pv.concat(cv);
+//   } else {
+//     return pv;
+//   }
+// }, []);
+
+// console.log(arr);
 
 
-console.log(res)
+// ---------------------------
+
+
+const words = ['spray', 'limit', 'exuberant', 'destruciton', 'abc']
