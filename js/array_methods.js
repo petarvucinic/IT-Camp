3f0df80470ad12a34a1e8fb8cba46920b575d48b
petarvucinic <87143624+petarvucinic@users.noreply.github.com>
2022-05-29 15:21:41 +0200

    commit

--- js/array_methods.js
+++ js/array_methods.js
@@ -96,17 +96,60 @@
 
 // --------------------
 
-const arr = [-3, -2, -1, 0, 1, 2, 3, 4, 7, 8, 9, 10, 11, 12, 13];
+// const arr = [-3, -2, -1, 0, 1, 2, 3, 4, 7, 8, 9, 10, 11, 12, 13];
 
-function prime(el) {
-  for (let i = 2; i < el; i++) {
-    if (el % i == 0) {
-      return false;
-    }
-  }
-  if (el > 0) return true;
-}
-const res = arr.filter((el) => {
-  return prime(el);
-});
-console.log(res);
+// function prime(el) {
+//   for (let i = 2; i < el; i++) {
+//     if (el % i == 0) {
+//       return false;
+//     }
+//   }
+//   if (el > 0) return true;
+// }
+// const res = arr.filter((el) => {
+//   return prime(el);
+// });
+// console.log(res);
+
+
+// -----------------------
+
+
+
+// ------------------------------------------------
+
+
+
+
+// const arr = [-3, -2, -1, 0, 1, 2, 3, 4, 7, 8, 9, 10, 11, 12, 13];
+
+// function prime(el) {
+//   for (let i = 2; i < el; i++) {
+//     if (el % i == 0) {
+//       return false;
+//     }
+//   }
+//   if (el > 1) return true;
+// }
+// const res = arr.find((el) => {
+//   return prime(el);
+// });
+// console.log(res);
+
+
+
+// -------------------------------------
+
+
+
+// every svi elementi ispunjavaju
+
+
+
+
+
+
+
+
+
+
