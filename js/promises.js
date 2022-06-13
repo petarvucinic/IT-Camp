263cfb1c6ff9fabaa03f3634cc19c7deab4639d5
petarvucinic <87143624+petarvucinic@users.noreply.github.com>
2022-06-13 20:29:02 +0200

    commit

--- js/promises.js
+++ js/promises.js
@@ -1,5 +1,7 @@
 // obecanje da ce se posao izvrsiti
 
+// const { reject } = require("async")
+
 // myProm = new Promise((res, rej) => {
 //   done = false;
 //   for (let i = 0; i < 90000; i++) {}
@@ -69,15 +71,97 @@
 
 // --------------------------------------------
 
-const sayHello = async () => {
-  console.log("hello!");
-};
+// const sayHello = async () => {
+//   console.log("hello!");
+// };
 
-const greeting = async () => {
-  await sayHello();
-  console.log("How are you?");
-};
-greeting();
-
+// const greeting = async () => {
+//   await sayHello();
+//   console.log("How are you?");
+// };
+// greeting();
 
 // ----------------------------------
+
+// const prvaF = () => {
+//   return new Promise((resolve, reject) => {
+//     a = ""
+//     for(i = 0; i < 100000; i++){ a === false ? pom = true : pom = false }
+//     if(pom) resolve("uspesno");
+//     else reject("nesupeh")
+//   })
+// }
+
+// prvaF()
+//       .then((message) => console.log("evo ga" + message))
+//       .catch((message) => console.log("ipak ne"))
+
+// +---------------------------------------
+
+// console.log("start");
+
+// function loginUser(email, password, callback) {
+//   setTimeout(() => {
+//     callback({ userEmail: email });
+//   }, 1500);
+// }
+
+// function getUserVideos(email, callback) {
+//   setTimeout(() => {
+//     callback(["video1", "video2", "video3"]);
+//   }, 1500);
+// }
+
+// function videoDetails(video, callback) {
+//   setTimeout(() => {
+//     callback("title of the video")
+//   }, 1500)
+// }
+
+// const user = loginUser("pero@gmail.com", "1223", (user) => {
+//   console.log(user)
+//   getUserVideos(user.userEmail, (videos) => {
+//     console.log(videos)
+//     videoDetails(videos[0], (title) => {
+//       console.log(title)
+//     })
+//   })
+// })
+
+
+// --------------------------------
+
+
+// console.log("Pocinjemo")
+
+
+// const prvaF = (ime, prezime, skola, razred, callback) => {
+//   setTimeout(() => {
+//     callback({ime : ime,
+//               prezime : prezime,
+//               skola : skola,
+//               razred : razred })
+//   }, 1110)
+// }
+
+// const drugaF = (ime, prezime, callback) => {
+//   setTimeout(() => {
+//     callback(["Mat, srp, nemacki"])
+//   }, 1500)
+// }
+
+// const ja = prvaF("Petar", "Vucinic", "Gimnazija", 3, (ja) => {
+//   console.log(ja)
+//   drugaF(ja.ime, ja.prezime, (predmeti) => {
+//     console.log(predmeti)
+//   })
+// })
+
+
+// console.log("Zavrsili smo")
+
+
+
+// -------------------------------
+
+
