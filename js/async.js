7487dbe3a33e1c25dd26e9a735704d58db79d6d6
petarvucinic <87143624+petarvucinic@users.noreply.github.com>
2022-06-13 13:04:15 +0200

    commit

--- js/async.js
+++ js/async.js
@@ -1,5 +1,36 @@
 
 
+// console.log("Start");
 
+// function loginUser(email, password, callback) {
+//   setTimeout(() => {
+//     callback({ userEmail: email });
+//   }, 1000);
+// }
+
+// function getUserVideos(email, callback) {
+//   setTimeout(() => {
+//     callback(["video1", "video2", "video3"]);
+//   }, 1000);
+// }
+
+// function videoDetails(video, callback) {
+//   setTimeout(() => {
+//     callback("title of the video");
+//   }, 1000);
+// }
+// const user = loginUser("cduc@gmail.com", 123, (user) => {
+//   console.log(user);
+//   getUserVideos(user.userEmail, (videos) => {
+//     console.log(videos);
+//     videoDetails(videos[0], (title) => {
+//       console.log(title);
+//     });
+//   });
+// });
+
+// console.log("Finish");
+//----------------------------------------------------------------------------------------------------------------------
+// Language: javascript promises
 
 
